b28acd6773a795cfb8f6092e79874cf4508280cc
frank <fhasenbalg@it-kontor.com>
2019-04-01 09:12:44 +0200

    add self timer

--- SD-card-root/www/js/main.js
+++ SD-card-root/www/js/main.js
@@ -19,15 +19,37 @@ function loadImgs() {
     xhttp.send();
 }
 
+function takePicture() {
+    document.getElementById("btn-shutter").disabled = true;
+
+    let xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            document.getElementById("btn-shutter").innerHTML = 'Taking Picture&hellip;';
+            setTimeout(function () {
+                document.getElementById("btn-shutter").innerHTML = 'Take a Picture';
+                document.getElementById("btn-shutter").disabled = false;
+            }, 12000);
+
+        }
+    };
+    setTimeout(function () {
+        xhttp.open("GET", "/cgi-bin/shutter-button", true);
+        xhttp.send();
+    }, document.getElementById('timer-time').value * 1000);
+
+
+}
+
 function setISO() {
     let xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             console.log(this.responseText.split(' '));
             let reponse = this.responseText.split(' ');
-            console.log(reponse[reponse.length -1].replace('(', '').replace(')', '').trim());
+            console.log(reponse[reponse.length - 1].replace('(', '').replace(')', '').trim());
 
-            document.getElementById('iso_dropdown').value = reponse[reponse.length -1].replace('(', '').replace(')', '').trim();
+            document.getElementById('iso_dropdown').value = reponse[reponse.length - 1].replace('(', '').replace(')', '').trim();
         }
     };
     xhttp.open("POST", "/cgi-bin/set_iso", true);
@@ -40,6 +62,7 @@ function init() {
     document.getElementById("iso_dropdown").addEventListener("change", setISO);
     setISO();
 
+    document.getElementById("btn-shutter").addEventListener('click', takePicture);
     document.getElementById("btn-refresh").addEventListener('click', loadImgs);
 }
 
